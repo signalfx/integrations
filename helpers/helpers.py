964cc7a19c141a2065733e382778da87980fc60d
Ben Keith <bkeith@signalfx.com>
2019-09-05 17:21:48 -0400

    Fix encoding issues in to-product-docs
    Also update Dockerfile to work with to-product-docs script

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,5 +1,8 @@
+import pathlib
 import re
+
 import yaml
+
 
 def extract(content, starter, stopper):
     lines = content.splitlines(True)
@@ -7,32 +10,32 @@ def extract(content, starter, stopper):
     if starter == None:
         started = True
 
-    extracted = ''
+    extracted = ""
     for line in lines:
         if started == False and line.strip().startswith(starter):
             started = True
             continue
         elif len(line.split()) > 0 and started and stopper and line.strip().startswith(stopper):
             break
-        
+
         if started:
             extracted += line
     return extracted
 
+
 def get_file_contents(file_path):
-    readme = open(file_path, 'r')
-    contents = readme.read()
-    readme.close()
-    return str(contents)
+    return pathlib.Path(file_path).read_text(encoding="utf-8")
+
 
 def get_output_filename(monitor, header):
-    output_filename = re.sub(r'\!\[[^\[\]]*\]\(([^\(\)]*)\)', '', header).split('|')[-1].replace('#', '')
+    output_filename = re.sub(r"\!\[[^\[\]]*\]\(([^\(\)]*)\)", "", header).split("|")[-1].replace("#", "")
     output_filename = output_filename.strip().lower()
-    output_filename = output_filename.replace(' ', '.').replace('/', '.')
-    if output_filename == '':
+    output_filename = output_filename.replace(" ", ".").replace("/", ".")
+    if output_filename == "":
         output_filename = monitor
-    output_filename = 'integrations.' + output_filename + '.md'
+    output_filename = "integrations." + output_filename + ".md"
     return output_filename
+
 
 def collect_metrics_yaml(file_path):
     contents = get_file_contents(file_path)
@@ -41,19 +44,16 @@ def collect_metrics_yaml(file_path):
     for key in parsed:
         metric = parsed.get(key)
         metric_name = key
-        brief = ''
-        description = ''
-        if metric.get('brief'):
-            brief = metric.get('brief')
-        if metric.get('description'):
-            description = metric.get('description')
-        metric_type = ''
-        if metric.get('metric_type'):
-            metric_type = metric.get('metric_type')
-        metrics.append({
-            'metric_name': metric_name,
-            'brief': brief,
-            'description': description,
-            'metric_type': metric_type
-        })
+        brief = ""
+        description = ""
+        if metric.get("brief"):
+            brief = metric.get("brief")
+        if metric.get("description"):
+            description = metric.get("description")
+        metric_type = ""
+        if metric.get("metric_type"):
+            metric_type = metric.get("metric_type")
+        metrics.append(
+            {"metric_name": metric_name, "brief": brief, "description": description, "metric_type": metric_type}
+        )
     return metrics
